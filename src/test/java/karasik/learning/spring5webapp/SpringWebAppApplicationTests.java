f060f049742686ea02a4c5f30641a8b5512d85f7
Oleksiy <maksymenko1111@gmail.com>
2021-01-19 21:04:51 +0200

    Initialzing project

---
+++ src/test/java/karasik/learning/spring5webapp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package karasik.learning.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
